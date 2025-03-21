1cd90c224f1513df3b0b79e392d4bf9c54e2c40d
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-03-21 09:48:12 +0900

    chore: update package.json and commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,12 +1,11 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [
+    'type-enum': [
       2,
-      "always",
-      ["feat", "fix", "docs", "style", "refactor", "test", "chore"],
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
     ],
-    "header-max-length": [0],
-    "references-empty": [2, "never"],
+    'header-max-length': [0],
   },
 };
