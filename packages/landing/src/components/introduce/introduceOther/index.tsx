bde677440a986030f100c8380b4ca590113997be
officeshinyujun <officeshinyujun@gmail.com>
2025-03-23 04:40:01 +0900

    feat: add introduce other section

--- packages/landing/src/components/introduce/introduceOther/index.tsx
+++ packages/landing/src/components/introduce/introduceOther/index.tsx
@@ -1,23 +1,24 @@
 import {Hstack} from "../../Hstack";
-import {Vstack} from "../../Vstack";
 import IntroduceProduct from "../introduceProduct";
 import selfHostLogo from "../../../assets/self-host-logo.svg";
 import designKitLogo from "../../../assets/design-kit-logo.svg";
 import jsonMockLogo from "../../../assets/json-mock-logo.svg";
 import serverLogo from "../../../assets/server-logo.svg";
+import testImage from "../../../assets/testImage.png";
 import s from "./style.module.scss"
+import {Vstack} from "../../Vstack";
 
 export default function IntroduceOtherSection() {
     return (
-        <Hstack className={s.container}>
-            <Vstack gap ={24} align={"center"} justify={"center"}>
+        <Vstack className={s.container} align={"center"} justify={"center"}>
+            <Hstack gap ={24} align={"center"} justify={"center"}>
                 <IntroduceProduct
                 img={selfHostLogo}
                 title={"Self-Hosted"}
                 borderColor={"#FF9201"}
                 backgroundColor={"#FFF4E0"}
                 isBorder={true}
-                rotating={30}
+                rotating={-3}
             />
                 <IntroduceProduct
                     img={designKitLogo}
@@ -25,7 +26,7 @@ export default function IntroduceOtherSection() {
                     borderColor={"#6E9F27"}
                     backgroundColor={"#EEF5E1"}
                     isBorder={false}
-                    rotating={30}
+                    rotating={3}
                 />
                 <IntroduceProduct
                     img={jsonMockLogo}
@@ -33,7 +34,7 @@ export default function IntroduceOtherSection() {
                     borderColor={"#8D5CFF"}
                     backgroundColor={"#F1E9FF"}
                     isBorder={true}
-                    rotating={30}
+                    rotating={3}
                 />
                 <IntroduceProduct
                     img={serverLogo}
@@ -41,10 +42,10 @@ export default function IntroduceOtherSection() {
                     borderColor={"#169FB0"}
                     backgroundColor={"#E1F7F9"}
                     isBorder={false}
-                    rotating={30}
+                    rotating={-3}
                 />
-            </Vstack>
-
-        </Hstack>
+            </Hstack>
+            <img src={testImage} className={s.img}/>
+        </Vstack>
     )
 }
