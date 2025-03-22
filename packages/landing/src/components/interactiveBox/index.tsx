0bc3778520a4b39c3853e6d9eed86c6ba5f6489d
officeshinyujun <officeshinyujun@gmail.com>
2025-03-23 03:14:29 +0900

    feat: add interactiveBox

---
+++ packages/landing/src/components/interactiveBox/index.tsx
@@ -0,0 +1,19 @@
+import {Hstack} from "../Hstack";
+
+interface InteractiveBoxProps {
+    className?: string;
+    text : string;
+    functionImage?: string;
+}
+
+export default function InteractiveBox(props: InteractiveBoxProps) {
+    const {className, text, functionImage} = props;
+    return(
+        <Hstack align={"center"} className = {className}>
+            <p>{text}</p>
+            {functionImage && (
+                <img src={functionImage} alt='기능 이미지'/>
+            )}
+        </Hstack>
+    )
+}
